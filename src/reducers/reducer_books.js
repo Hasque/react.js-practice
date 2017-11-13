41cdb30f03e47b4ce114fbc361eb7233244e1b05
Hasque <hasque@gmail.com>
2017-11-12 21:12:23 -0500

    Working BookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+	return [
+		{title: 'Javascript: the Good parts' },
+		{title: 'Harry potter' },
+		{title: 'The Dark Tower' },
+		{title: 'Eloquent Ruby' }
+
+	]
+}
+
